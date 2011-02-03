82558ee960149eca152e8cf1c0026d86de9ff0b6
Stuart Yeates <syeates@gmail.com>
2011-02-04 06:06:05 +1300

    use tests

---
+++ src/com/github/stuartyeates/TEIDictLeitner/ModelTest.java
@@ -0,0 +1,19 @@
+package com.github.stuartyeates.TEIDictLeitner;
+
+import junit.framework.TestCase;
+
+public class ModelTest extends TestCase {
+
+	public ModelTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
